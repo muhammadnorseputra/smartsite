2cd7ea2e2a734d71ab0e1eabccea62f6d896dfd2
muhammadnorseputra <muhammadnorseputra@gmail.com>
2021-01-20 10:00:22 +0700

    bug error

--- application/models/M_b_admin.php
+++ application/models/M_b_admin.php
@@ -1,5 +1,4 @@
-
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if(!defined('BASEPATH')) exit('No direct script access allowed');
 
 class M_b_admin extends CI_Model {
 
