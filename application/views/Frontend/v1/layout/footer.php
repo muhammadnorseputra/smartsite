e3122921776fa1ebb79a447f14013a4926459c63
muhammadnorseputra <muhammadnorseputra@gmail.com>
2021-01-21 15:38:06 +0700

    update ui frontend

--- application/views/Frontend/v1/layout/footer.php
+++ application/views/Frontend/v1/layout/footer.php
@@ -3,7 +3,7 @@
 		<div class="row border-top">
 			<div class="col-md-12">
 				<div class="d-block p-4">
-					<div class="container text-center w-50">
+					<div class="container text-center">
 						<?php echo '<img src="data:image/jpeg;base64,' . base64_encode($mf_beranda->site_logo) . '" width="140"/>'; ?>
 						<br>
 						<div class="text-muted mt-3">&copy; <?php echo date('Y') ?> Bkppd kab. Balangan</div>
