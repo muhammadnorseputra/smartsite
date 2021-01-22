9ad551b05ee591e0ebe2c1c71933d7a823ed5c41
muhammadnorseputra <muhammadnorseputra@gmail.com>
2021-01-22 19:01:12 +0700

    fix cors origin https ke http

--- template/v1/prod/app.js
+++ template/v1/prod/app.js
@@ -803,7 +803,15 @@ $(document).ready(function () {
 });
 "use strict";
 
-// Uri Segement
+// Mengijinkan cors origin dari https ke http
+if (window.location.protocol.indexOf('https') == 0) {
+  var el = document.createElement('meta');
+  el.setAttribute('http-equiv', 'Content-Security-Policy');
+  el.setAttribute('content', 'upgrade-insecure-requests');
+  document.head.append(el);
+} // Uri Segement
+
+
 var _uri = "".concat(window.location.origin);
 
 var _uriSegment = window.location.pathname.split('/');
