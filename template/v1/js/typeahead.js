f4f32e9ed36d3a13877704dbe821485c66767f4f
muhammadnorseputra <muhammadnorseputra@gmail.com>
2021-01-24 17:44:27 +0700

    fix seacrh pegawai

--- template/v1/js/typeahead.js
+++ template/v1/js/typeahead.js
@@ -30,6 +30,8 @@ $(document).ready(function() {
                         type: "POST",
                         url: "http://silka.bkppd-balangankab.info/api/filternipnama",
                         dataType: "json",
+                        crossDomain: true,
+                        async: true,
                         data: {
                             q: "{{query}}",
                         }
--- template/v1/prod/app.js
+++ template/v1/prod/app.js
@@ -921,6 +921,8 @@ $(document).ready(function () {
             type: "POST",
             url: "http://silka.bkppd-balangankab.info/api/filternipnama",
             dataType: "json",
+            crossDomain: true,
+            async: true,
             data: {
               q: "{{query}}"
             }
