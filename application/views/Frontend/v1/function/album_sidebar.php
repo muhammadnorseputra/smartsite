81bf8f4d92081f8af4f21bb838f893d731f23c67
muhammadnorseputra <muhammadnorseputra@gmail.com>
2021-04-26 08:37:48 +0700

    fid bug album sidebar

--- application/views/Frontend/v1/function/album_sidebar.php
+++ application/views/Frontend/v1/function/album_sidebar.php
@@ -13,7 +13,7 @@
                 <?php else: ?>
                 <img data-src="<?= img_blob($album->gambar_blob) ?>" class="img-fluid w-100 lazy rounded shadow-sm border " alt="<?= url_title($album->judul, '-', true) ?>">
                 <?php endif; ?>
-                <div class="text-secondary float-right badge-light p-2 rounded mt-2"><?= $this->album->jml_photo_in_album($album->id_album_foto) ?> Photo</div> 
+                <div class="text-secondary text-center badge-light p-2 rounded mt-2"><?= $this->album->jml_photo_in_album($album->id_album_foto) ?> Photo</div> 
                 <p class="text-muted mt-3"><?= $album->judul ?></p>
             </a>
             </div>
