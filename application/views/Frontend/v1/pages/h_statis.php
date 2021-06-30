669d305fdb5fc893c77bc2aeadc498cfe6226e34
muhammadnorseputra <muhammadnorseputra@gmail.com>
2021-06-30 13:17:47 +0700

    remove ads

--- application/views/Frontend/v1/pages/h_statis.php
+++ application/views/Frontend/v1/pages/h_statis.php
@@ -53,17 +53,6 @@
         <img src="data:image/jpeg;base64,<?= base64_encode($h->file) ?>" alt="<?= $h->filename ?>" class="mx-auto img-fluid d-block">
         <?php endif; ?>
       <?php endif; ?>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        <!-- ads-display-->
-        <ins class="adsbygoogle"
-             style="display:block"
-             data-ad-client="ca-pub-1099792537777374"
-             data-ad-slot="5114117650"
-             data-ad-format="auto"
-             data-full-width-responsive="true"></ins>
-        <script>
-             (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
       </div>
     </div>
   </div>
