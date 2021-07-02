676ec7f4d201a70d3c43c297260689ab6f0e2176
muhammadnorseputra <muhammadnorseputra@gmail.com>
2021-07-02 13:13:49 +0700

    add image in rssFeed

--- application/views/Frontend/v1/rss.php
+++ application/views/Frontend/v1/rss.php
@@ -1,22 +1,15 @@
 <?php  echo '<?xml version="1.0" encoding="' . $encoding . '"?>' . "\n"; ?>
-<rss version="2.0"
-    xmlns:dc="http://purl.org/dc/elements/1.1/"
-    xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
-    xmlns:admin="http://webns.net/mvcb/"
-    xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-    xmlns:content="http://purl.org/rss/1.0/modules/content/"
-    xmlns:atom="http://www.w3.org/2005/Atom">
+<rss version="2.0">
  
     <channel>
-    <atom:link rel="self" type="application/rss+xml" href="<?php echo $feed_url; ?>"/>
     <title><?php echo $feed_name; ?></title>
  
     <link><?php echo $feed_url; ?></link>
     <description><?php echo $page_description; ?></description>
-    <dc:language><?php echo $page_language; ?></dc:language>
-    <dc:creator><?php echo $creator_email; ?></dc:creator>
+    <language><?php echo $page_language; ?></language>
+    <creator><?php echo $creator_email; ?></creator>
  
-    <dc:rights>Copyright <?php echo gmdate("Y", time()); ?></dc:rights>
+    <rights>Copyright <?php echo gmdate("Y", time()); ?></rights>
     <admin:generatorAgent rdf:resource="http://www.codeigniter.com/" />
  
     <?php 
