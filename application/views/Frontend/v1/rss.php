d7c4ca65830795d44f97d707f6584e2a1d4eab1c
muhammadnorseputra <muhammadnorseputra@gmail.com>
2021-07-02 12:43:39 +0700

    add atom:link in rssFeed

--- application/views/Frontend/v1/rss.php
+++ application/views/Frontend/v1/rss.php
@@ -1,5 +1,11 @@
 <?php  echo '<?xml version="1.0" encoding="' . $encoding . '"?>' . "\n"; ?>
-<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+<rss version="2.0"
+    xmlns:dc="http://purl.org/dc/elements/1.1/"
+    xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
+    xmlns:admin="http://webns.net/mvcb/"
+    xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+    xmlns:content="http://purl.org/rss/1.0/modules/content/"
+    xmlns:atom="http://www.w3.org/2005/Atom">
  
     <channel>
     <atom:link rel="self" type="application/rss+xml" href="<?php echo $feed_url; ?>"/>
