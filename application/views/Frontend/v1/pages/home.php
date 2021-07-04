08a4eea18db66a8c356b1e74d762ce12567ee257
muhammadnorseputra <muhammadnorseputra@gmail.com>
2021-07-04 13:08:06 +0700

    code ads

--- application/views/Frontend/v1/pages/home.php
+++ application/views/Frontend/v1/pages/home.php
@@ -83,16 +83,16 @@
                 <div class="text-center">
                     <button id="load_more" class="btn p-2 btn-primary btn-block rounded-lg px-4"><i class="fas fa-newspaper mr-2"></i> Loadmore</button>
                 </div>
-                <!-- <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                 <ins class="adsbygoogle"
-                style="display:block"
-                data-ad-format="fluid"
-                data-ad-layout-key="-f9+4v+7r-fc+65"
-                data-ad-client="ca-pub-1099792537777374"
-                data-ad-slot="5769488115"></ins>
+                    style="display:block"
+                    data-ad-format="fluid"
+                    data-ad-layout-key="-f9+4v+7r-fc+65"
+                    data-ad-client="ca-pub-1099792537777374"
+                    data-ad-slot="5769488115"></ins>
                 <script>
                 (adsbygoogle = window.adsbygoogle || []).push({});
-                </script> -->
+                </script>
             </div>
         </div>
     </div>
