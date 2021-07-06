6c9cd137791dc9a2274422f5fee4db536a34fbb0
muhammadnorseputra <muhammadnorseputra@gmail.com>
2021-07-06 10:51:43 +0700

    update rss

--- application/views/Frontend/v1/rss.php
+++ application/views/Frontend/v1/rss.php
@@ -37,10 +37,10 @@
       endif;
 
       $isi_berita = $post->content; // membuat paragraf pada isi berita dan mengabaikan tag html
-      $isi = substr($isi_berita, 0, 180); // ambil sebanyak 80 karakter
+      $isi = substr($isi_berita, 0, 200); // ambil sebanyak 80 karakter
       $isi = substr($isi_berita, 0, strrpos($isi, ' ')); // potong per spasi kalimat
-      $conditional = $post->tgl_posting === date('Y-m-d') ? 'New' : mediumdate_indo($post->tgl_posting);
-      $newDateTime= new DateTime($post->tgl_posting, new DateTimeZone('Asia/Jakarta'));;
+      $conditional = $post->tgl_posting === date('Y-m-d') ? 'Terbaru' : 'Lama';
+      $newDateTime= new DateTime($post->created_at, new DateTimeZone('Asia/Jakarta'));;
     ?>
         <item>
           <title><?php echo xml_convert($post->judul); ?></title>
@@ -56,7 +56,7 @@
           <g:id><?= $id ?></g:id>
           <guid isPermaLink="false"><?= $posturl ?></guid>
           <enclosure length="25000" type="image/jpeg" url="<?= $img ?>"/>
-          <pubDate><?= $newDateTime->format('D, d M Y G:i:sP') ?></pubDate>
+          <pubDate><?= $newDateTime->format('D, d M Y G:i:s P') ?></pubDate>
         </item>
     <?php endforeach; ?>
      
