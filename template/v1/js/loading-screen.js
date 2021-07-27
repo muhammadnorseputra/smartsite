ce1218df1bee5e6a9bbd59ce60ca341a329d42b7
muhammadnorseputra <muhammadnorseputra@gmail.com>
2021-07-27 14:00:07 +0700

    fix loading screen

--- template/v1/js/loading-screen.js
+++ template/v1/js/loading-screen.js
@@ -1,12 +1,12 @@
-// document.onreadystatechange = function() {
-//     if (document.readyState !== "complete") {
-//         // document.querySelector("html").style.visibility = "hidden";
-//         document.querySelector(".page-slider").style.transition = "0";
-//         document.querySelector(".page-slider").style.opacity = 1;
-//     } else {
-//         document.querySelector(".page-slider").style.transition = "0.5s";
-//         document.querySelector(".page-slider").style.opacity = 0;
-//         document.querySelector(".page-slider").style.visibility = "hidden";
-//         // document.querySelector("html").style.visibility = "visible";
-//     }
-// };
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        // document.querySelector("html").style.visibility = "hidden";
+        document.querySelector(".page-slider").style.transition = "0";
+        document.querySelector(".page-slider").style.opacity = 1;
+    } else {
+        document.querySelector(".page-slider").style.transition = "0.5s";
+        document.querySelector(".page-slider").style.opacity = 0;
+        document.querySelector(".page-slider").style.visibility = "hidden";
+        // document.querySelector("html").style.visibility = "visible";
+    }
+};
