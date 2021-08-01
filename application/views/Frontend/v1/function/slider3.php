433d8268b799c3d5566edc9be7ac3089f2ea5cbd
muhammadnorseputra <muhammadnorseputra@gmail.com>
2021-08-01 19:55:05 +0700

    fix seo

--- application/views/Frontend/v1/function/slider3.php
+++ application/views/Frontend/v1/function/slider3.php
@@ -10,19 +10,19 @@
                 <a href="#content-page" class="btn btn-info btn-block font-weight-lighter btn-lg animated fadeInDown scroll">Explore <i class="fas fa-arrow-down ml-3"></i></a>
                 <p class="mt-3 font-weight-bold animated fadeInUp">Follow Our Media</p>
                 <div class="d-flex justify-content-start align-items-center animated fadeInUp">
-                    <a  rel="noreferrer" target="_blank" href="<?= $mf_beranda->fb; ?>" class="btn py-3 btn-primary-old my-2  my-sm-0 animated fadeIn shadow-sm" data-toggle="tooltip" data-placement="bottom" title="Join group facebook">
+                    <a  rel="noreferrer" target="_blank" href="<?= $mf_beranda->fb; ?>" class="btn py-3 btn-primary-old my-2 my-sm-0 animated fadeIn shadow-sm" data-toggle="tooltip" data-placement="bottom" title="Join group facebook" alt="Join group facebook">
                         <i class="fab fa-facebook"></i>
                     </a>
-                    <a  rel="noreferrer" target="_blank" href="https://www.instagram.com/<?= $mf_beranda->ig; ?>" class="btn  py-3 btn-warning my-2 my-sm-0 mx-2 btn-instagram animated fadeIn shadow-sm" data-toggle="tooltip" data-placement="bottom" title="Follow Our Instagram" data-username="<?= $mf_beranda->ig; ?>">
+                    <a  rel="noreferrer" target="_blank" href="https://www.instagram.com/<?= $mf_beranda->ig; ?>" class="btn  py-3 btn-warning my-2 my-sm-0 mx-2 btn-instagram animated fadeIn shadow-sm" data-toggle="tooltip" data-placement="bottom" title="Follow Our Instagram" alt="Follow Our Instagram" data-username="<?= $mf_beranda->ig; ?>">
                         <i class="fab fa-instagram"></i>
                     </a>
-                    <a  rel="noreferrer" target="_blank" href="https://www.youtube.com/channel/UCFDRHqqNeuYql8O7y5sHgmw" class="btn py-3 btn-danger my-2 my-sm-0 btn-instagram animated fadeIn shadow-sm" data-toggle="tooltip" data-placement="bottom" title="Subscribe Channel Youtube">
+                    <a  rel="noreferrer" target="_blank" href="https://www.youtube.com/channel/UCFDRHqqNeuYql8O7y5sHgmw" class="btn py-3 btn-danger my-2 my-sm-0 btn-instagram animated fadeIn shadow-sm" data-toggle="tooltip" data-placement="bottom" title="Subscribe Channel Youtube" alt="Subscribe Channel Youtube">
                         <i class="fab fa-youtube"></i>
                     </a>
-                    <a  rel="noreferrer" target="_blank" href="mailto:bkppdbalangan@gmail.com" class="btn py-3 btn-success my-2 my-sm-0 mx-2 btn-instagram animated fadeIn shadow-sm" data-toggle="tooltip" data-placement="bottom" title="Email for works">
+                    <a  rel="noreferrer" target="_blank" href="mailto:bkppdbalangan@gmail.com" class="btn py-3 btn-success my-2 my-sm-0 mx-2 btn-instagram animated fadeIn shadow-sm" data-toggle="tooltip" data-placement="bottom" title="Email for works" alt="Email for works">
                         <i class="fas fa-envelope-open"></i>
                     </a>
-                    <a  rel="noreferrer" target="_blank" href="https://news.google.com/publications/CAAqBwgKMLfcpwswpOe_Aw?oc=3&ceid=ID:id" class="btn py-3 btn-info my-2 my-sm-0 btn-instagram animated fadeIn shadow-sm" data-toggle="tooltip" data-placement="bottom" title="Google News">
+                    <a  rel="noreferrer" target="_blank" href="https://news.google.com/publications/CAAqBwgKMLfcpwswpOe_Aw?oc=3&ceid=ID:id" class="btn py-3 btn-info my-2 my-sm-0 btn-instagram animated fadeIn shadow-sm" data-toggle="tooltip" data-placement="bottom" title="Google News" alt="Google News">
                         <i class="fab fa-google"></i>
                     </a>
                 </div>
