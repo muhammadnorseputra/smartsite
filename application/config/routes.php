3698f8bcb4d55e4029f5215d37c7404065cf1f8c
muhammadnorseputra <muhammadnorseputra@gmail.com>
2021-08-02 15:30:16 +0700

    fix logout

--- application/config/routes.php
+++ application/config/routes.php
@@ -99,6 +99,7 @@ $route['tag/(:any)'] = 'frontend/v1/post_list/tags/$1';
 $route['k/(:any)'] = 'frontend/v1/post_list/views/$1';
 $route['koran-online'] = 'frontend/v1/halaman/koran_online';
 $route['b/(:any)'] = 'frontend/v1/banner/detail/$1/$2';
+$route['logout'] = 'frontend/v1/users/logout';
 
 // Akun Login, Register
 $route['login_web'] = 'frontend/v1/users/login';
