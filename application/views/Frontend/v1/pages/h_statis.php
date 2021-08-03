25299f4f0dc2872a5671b4a1abb3b702bf786566
muhammadnorseputra <muhammadnorseputra@gmail.com>
2021-08-03 07:33:47 +0700

    fix hal statis

--- application/views/Frontend/v1/pages/h_statis.php
+++ application/views/Frontend/v1/pages/h_statis.php
@@ -63,6 +63,15 @@
         <img style="object-fit:cotain;" data-src="<?= img_blob($h->file) ?>" width="100%" height="350" alt="<?= $h->filename ?>" class="mx-auto img-fluid d-block rounded lazy">
         <?php endif; ?>
       <?php endif; ?>
+      <ins class="adsbygoogle"
+           style="display:block"
+           data-ad-client="ca-pub-1099792537777374"
+           data-ad-slot="5114117650"
+           data-ad-format="auto"
+           data-full-width-responsive="true"></ins>
+      <script>
+           (adsbygoogle = window.adsbygoogle || []).push({});
+      </script>
       </div>
     </div>
   </div>
