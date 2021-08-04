2d331b68848988ad262770dfbbcb8708599d2832
muhammadnorseputra <muhammadnorseputra@gmail.com>
2021-08-04 17:26:19 +0700

    fix amp

--- application/views/Frontend/amp/post/detail.php
+++ application/views/Frontend/amp/post/detail.php
@@ -16,7 +16,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 -->
 
-<html ⚡="">
+<html amp="amp">
   <head>
     <meta charset="utf-8" />
     <title><?= $title ?></title>
