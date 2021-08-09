41ad9b2e890d6966a1beee5a0cf8c8814cfb8f97
muhammadnorseputra <muhammadnorseputra@gmail.com>
2021-08-09 14:11:56 +0700

    created gpr_bkppdblg

--- application/controllers/frontend/v1/ApiPublic.php
+++ application/controllers/frontend/v1/ApiPublic.php
@@ -77,7 +77,7 @@ class ApiPublic extends RestController {
                     $by = $domain;
                 else:
                     $content = word_limiter($r->content, 25);
-                    $by=$namapanggilan;
+                    $by=ucwords($namalengkap);
                 endif;
 
                 // Post Link Detail
@@ -117,5 +117,101 @@ class ApiPublic extends RestController {
 	            ], 404 );
 		endif;
 	}
+    public function gpr_get()
+    {
+        $filter = [
+            'start'=> 0, 'limit' => 12, 'type'=>null, 'sort'=>null
+        ];
+        $data = $this->mf_beranda->get_all_berita($filter['limit'],$filter['start'],$filter['type'],$filter['sort']);
+        if ($data->num_rows() > 0):
+            $row = $data->result();
+            foreach($row as $r):
+                $by = $r->created_by;
+                if($by == 'admin') {
+                    $link_profile_public = 'javascript:void(0);';
+                    $namalengkap = $this->mf_users->get_namalengkap($by);
+                    $namapanggilan = $by;
+                    $gravatar = base_url('assets/images/users/'.$this->mf_users->get_gravatar($by));
+                } else {
+                    $link_profile_public = 
+                    base_url("user/".decrypt_url( $this->mf_users->get_userportal_namapanggilan($by)->nama_panggilan)."/".encrypt_url($by));
+                    $namalengkap = decrypt_url($this->mf_users->get_userportal_namalengkap($by));
+                    $namapanggilan = decrypt_url($this->mf_users->get_userportal_namapanggilan($by)->nama_panggilan);
+                    $gravatar = 'data:image/jpeg;base64,'.base64_encode($this->mf_users->get_userportal_byid($by)->photo_pic).'';
+                }
 
+                // Post Data Youtube
+                if($r->type === 'YOUTUBE'):
+                    $key      = $this->config->item('YOUTUBE_KEY'); // TOKEN goole developer
+                    $url      = 'https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&id='.$r->content.'&key='.$key;
+                    $yt     = api_client($url);
+                    $img = $yt['items'][0]['snippet']['thumbnails']['high']['url'];
+                    $yt_desc = $yt['items'][0]['snippet']['description'];
+                    $yt_src = $yt['items'][0]['snippet']['channelTitle'];
+                endif;
+
+                if($r->type === 'LINK'):
+                    $url = $r->content;
+                    $linker = getSiteOG($url);
+                    $img = $linker['image'];
+                endif;
+
+                if($r->type === 'BERITA'):
+                    if(empty($r->img)):
+                        $img = base_url('assets/images/noimage.gif');
+                    else:
+                       $img = base_url('files/file_berita/'.$r->img);
+                    endif;
+                endif;
+
+                // Content
+                if($r->type === 'YOUTUBE'):
+                    $content = word_limiter($yt_desc,25);
+                    $by = $yt_src;
+                elseif($r->type === 'LINK'):
+                    $content = word_limiter($linker['description'],25);
+                    $domain = parse_url($r->content, PHP_URL_HOST);
+                    $by = $domain;
+                else:
+                    $content = word_limiter($r->content, 25);
+                    $by=ucwords($namalengkap);
+                endif;
+
+                // Post Link Detail
+                if($r->type === 'YOUTUBE' || $r->type === 'BERITA'):
+                    $id = encrypt_url($r->id_berita);
+                    $postby = strtolower(url_title($namalengkap));
+                    $slug = strtolower($r->slug);
+                    $posturl = base_url("blog/{$slug}/");
+                else:
+                    $posturl = base_url('leave?go='.encrypt_url($r->content));
+                endif;
+
+                $datas = [
+                    'id_article' => $r->id_berita,
+                    'jdl_article' => $r->judul,
+                    'slug_article' => url_title(strtolower($r->judul)),
+                    'isi_article' => $content, 
+                    'img_article' => $img,
+                    'url_article' => $posturl,
+                    'tgl_posting_article' => longdate_indo($r->tgl_posting),
+                    'jml_comments_article' => $this->komentar->jml_komentarbyidberita($r->id_berita),
+                    'user_posting' => [
+                        'user_nama' => $by,
+                        'user_image' => blob_filename($gravatar),
+                        'user_link' => $link_profile_public
+                    ]
+                ];
+
+                $rs[] = $datas;
+            endforeach;
+            $json = $rs;
+            return $this->response( $json, 200 );
+        else:
+            return $this->response( [
+                    'status' => false,
+                    'message' => 'No article were found'
+                ], 404 );
+        endif;
+    }
 }
---
+++ assets/gpr/gpr_bkppdblg_dev.js
@@ -0,0 +1,110 @@
+/*
+how to use : 
+	<script type="text/javascript" src="https://web.bkppd-balangankab.info/assets/gpr/gpr_bkppdblg_dev.js"></script>
+	<div id="widget-gpr-bkppdblg"></div>
+*/
+(function() {
+
+// Localize jQuery variable
+var jQuery;
+var GPR_url = 'https://web.bkppd-balangankab.info/';
+// var GPR_url = 'http://localhost/smartsite/';
+
+/******** Load jQuery if not present *********/
+if (window.jQuery === undefined || window.jQuery.fn.jquery !== '1.12.4') {
+
+    var script_tag = document.createElement('script');
+    script_tag.setAttribute("type","text/javascript");
+    script_tag.setAttribute("src","//code.jquery.com/jquery-1.12.4.js");
+	
+    if (script_tag.readyState) {
+      script_tag.onreadystatechange = function () { // For old versions of IE
+          if (this.readyState == 'complete' || this.readyState == 'loaded') {
+              scriptLoadHandler();
+          }
+      };
+    } else { // Other browsers
+      script_tag.onload = scriptLoadHandler;
+    }
+    // Try to find the head, otherwise default to the documentElement
+    (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(script_tag);	
+} else {
+    // The jQuery version on the window is the one we want to use
+    jQuery = window.jQuery;
+    main();
+}
+
+/******** Called once jQuery has loaded ******/
+function scriptLoadHandler() {
+    // Restore $ and window.jQuery to their previous values and store the
+    // new jQuery in our local jQuery variable
+    jQuery = window.jQuery.noConflict(true);
+    // Call our main function
+    main(); 
+}
+
+/******** Our main function ********/
+function main() { 
+  jQuery(document).ready(function($) {
+		var css_link = $("<link>", { 
+			rel: "stylesheet",
+			type: "text/css",
+			href: GPR_url + "assets/gpr/gpr_bkppdblg.css"
+		});
+		css_link.appendTo('head');
+
+		//load 
+    var jsonp_url = GPR_url + "frontend/v1/apiPublic/gpr";
+
+    jQuery.getJSON(jsonp_url, function(data) {
+		console.log(data);
+		
+		var myhtml='<div class="gpr_bkppdblg">';
+				myhtml+='<div class="gpr_panel_head">';
+					myhtml+='<div class="gpr_panel_head_left">';
+						myhtml+='<img class="gpr_panel_head_img" src="'+GPR_url+'/assets/images/logo.png">';
+					myhtml+='</div>';
+					myhtml+='<div class="gpr_panel_head_right">';
+						myhtml+='<div class="gpr_panel_title">';
+							myhtml+='<div class="gpr_panel_title_main"><strong>GPR</strong> - BKPPD BLG </div>';
+							myhtml+='<div class="gpr_panel_title_sub"><em>Government Public Relations</em></div>';
+							myhtml+='<div class="gpr_panel_title_url"><a  rel="nofollow" href="https://web.bkppd-balangankab.info/beranda" target="_blank">https://web.bkppd-balangankab.info/</a></div>';
+						myhtml+='</div>';
+					myhtml+='</div>';			
+				myhtml+='</div>';
+
+			myhtml+='<ul class="gpr_list">';
+			jQuery.each(data, function(k, v) {
+				myhtml+='<li class="gpr_item">';
+					myhtml+='<div class="gpr_panel_left">';
+						myhtml+='<img class="gpr_panel_img" src="'+v.img_article+'">';
+					myhtml+='</div>';
+					myhtml+='<div class="gpr_panel_right">';
+						//myhtml+='<div class="gpr_panel_news_attr">';
+							//diindonesiakan ?
+							myhtml+='<div class="gpr_tgl">'+v.tgl_posting_article+' &bull; '+v.user_posting.user_nama+'</div>';
+							// myhtml+='<div class="gpr_kategori">Berita Terbaru</div>';
+						//myhtml+='</div>';
+						myhtml+='<br />';
+						
+						myhtml+='<a class="gpr_panel_news_title" href="'+v.url_article+'" target="_blank">';
+							myhtml+=v.jdl_article;
+						myhtml+='</a>';
+						
+					myhtml+='</div>';
+				myhtml+='</li>';
+			});
+			myhtml+='</ul>';
+			myhtml+='<div class="gpr_footer">';
+				myhtml+='<a rel="nofollow" href="https://web.bkppd-balangankab.info/beranda?type=all&sort=newest" target="_blank">More +</a>';
+			myhtml+='</div>';
+		myhtml+='</div>';
+			
+			jQuery('#widget-gpr-bkppdblg').html(myhtml);
+			
+    });
+
+	});
+}
+
+})(); // We call our anonymous function immediately
