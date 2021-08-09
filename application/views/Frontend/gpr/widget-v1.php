288bcbedb22f08b296ab3a5c5ff51b46532b7851
muhammadnorseputra <muhammadnorseputra@gmail.com>
2021-08-09 21:06:07 +0700

    update gpr

--- application/views/Frontend/gpr/widget-v1.php
+++ application/views/Frontend/gpr/widget-v1.php
@@ -5,7 +5,7 @@
 				
 			</div>
 			<div class="col-md-4">
-				<script defer crossorigin="anonymous" type="text/javascript" src="<?= assets("gpr/gpr_bkppdblg_dev.js") ?>"></script>
+				<script defer crossorigin="anonymous" type="text/javascript" src="<?= assets("gpr/gpr_bkppdblg_production.js") ?>"></script>
 				<div id="widget-gpr-bkppdblg"></div>
 			</div>	
 		</div>
