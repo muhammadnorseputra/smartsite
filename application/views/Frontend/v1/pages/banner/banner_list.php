fb70c4caa2577ed83294a0e684a607f2e7553f9a
muhammadnorseputra <muhammadnorseputra@gmail.com>
2021-08-13 20:19:35 +0700

    Update UI Bannerlist

--- application/views/Frontend/v1/pages/banner/banner_list.php
+++ application/views/Frontend/v1/pages/banner/banner_list.php
@@ -1,4 +1,4 @@
-<script src="<?= base_url('bower_components\list.js\dist\list.min.js') ?>"></script>
+<script async src="<?= base_url('bower_components\list.js\dist\list.min.js') ?>"></script>
 <section class="hero py-5">
 	<div class="container pt-md-5">
 		<div class="d-flex justify-content-between align-items-center">
@@ -26,19 +26,26 @@
 				  </button>
 				</div>
 			</div>
-			<ul class="list-group list-group-flush shadow rounded list" style="max-height: 540px; overflow-y: scroll;">
-				<?php foreach ($banner as $b): ?>
-				<a href="<?= base_url('b/'.$b->slug); ?>" class="list-group-item list-group-item-action">
-					<div class="d-flex justify-content-lg-start align-items-center flex-lg-row flex-column">
-						<div class="w-25">
-							<img data-src="<?= $b->path; ?>" class="lazy rounded img-fluid">
-						</div>
-						<div class="ml-3">
+			<ul class="card-columns pl-0">
+				<?php 
+					foreach ($banner as $b): 
+					if($b->fid_jns_banner === '38'):
+						$flexDir = 'flex-column';
+					else:
+						$flexDir = 'flex-row';
+					endif;
+				?>
+				<div class="card">
+				<a href="<?= base_url('b/'.$b->slug); ?>" class="">
+					<div class="d-flex justify-content-lg-start align-items-center <?= $flexDir ?>">
+						<img data-src="<?= $b->path; ?>" class="lazy card-img-top">
+												<div class="ml-3">
 							<b class="title_banner">#<?php echo $b->judul; ?></b>
 							<p class="text-muted small"><?php echo word_limiter($b->keterangan, 10); ?></p>
 						</div>
 					</div>
 				</a>
+				</div>
 				<?php endforeach; ?>
 			</ul>
 		</div>
