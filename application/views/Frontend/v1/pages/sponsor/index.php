3c766c290d7428c38e8d15ce5e008c12dee73fdc
muhammadnorseputra <muhammadnorseputra@gmail.com>
2021-08-24 14:59:12 +0700

    ad adsttera

--- application/views/Frontend/v1/pages/sponsor/index.php
+++ application/views/Frontend/v1/pages/sponsor/index.php
@@ -2,15 +2,17 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-12 pt-md-3">
-				<div id="SC_TBlock_858781"></div>
 			</div>
 		</div>
 	</div>
 </section>
 <script type="text/javascript">
-(sc_adv_out = window.sc_adv_out || []).push({
-    id: 858781,
-    domain: "n.ads5-adnow.com",
-});
+	atOptions = {
+		'key' : 'c751086761f46fec2b8094f16fffab8e',
+		'format' : 'iframe',
+		'height' : 300,
+		'width' : 360,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://overlapflintsidenote.com/c751086761f46fec2b8094f16fffab8e/invoke.js"></scr' + 'ipt>');
 </script>
-<script type="text/javascript" src="//st-n.ads5-adnow.com/js/a.js" async></script>
