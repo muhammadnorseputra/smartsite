a0be947d97ee814608ab51f069c9bdb535b6c2d5
muhammadnorseputra <muhammadnorseputra@gmail.com>
2021-08-24 15:09:41 +0700

    ad adsttera

--- application/views/Frontend/v1/pages/sponsor/index.php
+++ application/views/Frontend/v1/pages/sponsor/index.php
@@ -1,18 +1,22 @@
 <section class="mt-md-5">
 	<div class="container">
 		<div class="row">
-			<div class="col-12 pt-md-3">
+			<div class="col-12 pt-md-3 text-center">
+				<h1 class="text-muted">Sponsor Belum Ada</h1>
+				<p>
+					Silahkan Contact Administrator <a href="mailto:muhammadnorseputra@gmail.com">my@develover</a>
+				</p>
+				<script type="text/javascript">
+					atOptions = {
+						'key' : 'c751086761f46fec2b8094f16fffab8e',
+						'format' : 'iframe',
+						'height' : 300,
+						'width' : 360,
+						'params' : {}
+					};
+					document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://overlapflintsidenote.com/c751086761f46fec2b8094f16fffab8e/invoke.js"></scr' + 'ipt>');
+				</script>
 			</div>
 		</div>
 	</div>
 </section>
-<script type="text/javascript">
-	atOptions = {
-		'key' : 'c751086761f46fec2b8094f16fffab8e',
-		'format' : 'iframe',
-		'height' : 300,
-		'width' : 360,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://overlapflintsidenote.com/c751086761f46fec2b8094f16fffab8e/invoke.js"></scr' + 'ipt>');
-</script>
