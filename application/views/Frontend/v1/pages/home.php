bc7dbb4f0f73d07db10d8e08863cfb0f5ca33943
muhammadnorseputra <muhammadnorseputra@gmail.com>
2021-08-25 18:26:12 +0700

    up koran-online

--- application/views/Frontend/v1/pages/home.php
+++ application/views/Frontend/v1/pages/home.php
@@ -60,7 +60,7 @@
             <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 order-last order-md-last mt-4 mt-md-0">
                 <div id="sidebar">
                 
-                <a rel="noreferrer, nofollow" target="_blank" href="https://www.buymeacoffee.com/putrabungsu6"><img class="w-100" src="https://img.buymeacoffee.com/button-api/?text=Support My Developer&emoji=&slug=putrabungsu6&button_colour=40DCA5&font_colour=ffffff&font_family=Cookie&outline_colour=000000&coffee_colour=FFDD00" alt="putrabungsu6"></a>
+                <a href="https://www.buymeacoffee.com/putrabungsu6"><img src="https://img.buymeacoffee.com/button-api/?text=Donasi coffee untuk dev&emoji=&slug=putrabungsu6&button_colour=FFDD00&font_colour=000000&font_family=Inter&outline_colour=000000&coffee_colour=ffffff" class="w-100"></a>
 
                 <a href="<?= base_url('koran-online'); ?>" class="btn btn-outline-light btn-light py-3 my-3 btn-block">
                 <div class="d-flex justify-content-between align-items-center text-primary">
