2516347e83a17a368eb33c41cb97ea88488407dd
muhammadnorseputra <muhammadnorseputra@gmail.com>
2021-08-25 18:10:31 +0700

    up koran-online

--- application/views/Frontend/v1/pages/koran/index.php
+++ application/views/Frontend/v1/pages/koran/index.php
@@ -1,11 +1,28 @@
 <section class="mt-md-5">
 	<div class="container">
 		<div class="row">
-			<div class="col-12 pt-md-5">
+			<div class="col-2">
+				<div id="sidebar">
+				<!-- Composite Start -->
+				<div id="M724001ScriptRootC1189451"></div>
+				<script src="https://jsc.mgid.com/w/e/web.bkppd-balangankab.info.1189451.js" async></script>
+				<!-- Composite End -->
+				</div>
+			</div>
+			<div class="col-8 pt-md-5" id="main-content">
 				<!-- Composite Start -->
 				<div id="M724001ScriptRootC1169822"></div>
 				<script src="https://jsc.mgid.com/w/e/web.bkppd-balangankab.info.1169822.js" async></script>
 				<!-- Composite End -->
+			</div>
+			<div class="col-2">
+
+				<div id="sidebar">
+				<!-- Composite Start -->
+				<div id="M724001ScriptRootC1189453"></div>
+				<script src="https://jsc.mgid.com/w/e/web.bkppd-balangankab.info.1189453.js" async></script>
+				<!-- Composite End -->
+				</div>
 			</div>
 		</div>
 	</div>
