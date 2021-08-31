91dedfced4b72198f4351dc73bf9571981d6d5f2
muhammadnorseputra <muhammadnorseputra@gmail.com>
2021-08-31 14:55:57 +0700

    up home

--- application/views/Frontend/v1/layout/content.php
+++ application/views/Frontend/v1/layout/content.php
@@ -5,9 +5,10 @@ if ($isi) {
 }
 ?>
 
-<div class="fixed-bottom ml-auto text-center w-5 mb-5 mb-md-0 pb-4 mr-4">
-    <button aria-hidden="true" type="button" aria-label="button" title="Back to top" class="btn shadow btn-primary border-0 shadow-sm text-center rounded btn-backtop rippler rippler-inverse mb-3"><i class="fas fa-arrow-up px-1 py-2"></i></button>
+<div class="fixed-bottom ml-auto text-center w-5 mb-5 mb-md-0 pb-4 mr-5">
+    <button aria-hidden="true" type="button" aria-label="button" title="Back to top" class="btn shadow btn-primary btn-lg border-0 shadow-sm text-center rounded-circle btn-backtop rippler rippler-inverse mb-3"><i class="fas fa-arrow-up px-1 py-2"></i></button>
 </div>
+<script data-name="NBJ-Widget" data-cfasync="false" src="https://www.nihbuatjajan.com/javascripts/widget.prod.min.js" data-id="putra" data-domain="https://www.nihbuatjajan.com" data-description="" data-message="" data-color="#FF813F" data-position="left" data-x_margin="20" data-y_margin="90"></script>
 <?php if($this->mf_beranda->get_identitas()->status_maintenance == '1') { ?>
  <div class="alert alert-danger fixed-bottom rounded-0 py-2 m-0" role="alert">
     <div class="container text-center">
