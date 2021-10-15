9409444725f7343347b4b20b3c5a353f71acbe8c
muhammadnorseputra <muhammadnorseputra@gmail.com>
2021-10-15 10:31:45 +0700

    update statistisi

--- application/models/M_b_statistik.php
+++ application/models/M_b_statistik.php
@@ -64,9 +64,10 @@ class M_b_statistik extends CI_Model {
   public function ip_hits($tgl_m,$tgl_s,$lvl=null)
   {
     if($lvl == 'up'):
-      $this->db->select('ip');
+      $this->db->select_max('ip');
       $this->db->select_max('hits');
     else:
+      $this->db->select_min('ip');
       $this->db->select_min('hits');
     endif;
     $this->db->from($this->table);
