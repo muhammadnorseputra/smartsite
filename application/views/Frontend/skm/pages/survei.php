02b3744dab042f6e5e2e437ee669c6d8a86830bf
muhammadnorseputra <muhammadnorseputra@gmail.com>
2021-10-28 07:22:27 +0700

    up skm

--- application/views/Frontend/skm/pages/survei.php
+++ application/views/Frontend/skm/pages/survei.php
@@ -90,8 +90,8 @@
 				<svg xmlns="http://www.w3.org/2000/svg" width="150" height="150" fill="currentColor" class="bi bi-person-badge-fill text-secondary" viewBox="0 0 16 16">
 					<path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm4.5 0a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zM8 11a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm5 2.755C12.146 12.825 10.623 12 8 12s-4.146.826-5 1.755V14a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-.245z"/>
 				</svg>
-				<h3 class="my-4 fw-bold">BKPSDM BALANGAN</h3>
-				<p class="text-muted">Formulir Elektronik IKM (Indeks Kepuasan Masyarakat) Badan Kepegawaian dan Pengembangan Sumber Daya Manusia</p>
+				<h3 class="my-4 fw-bold">BKPPD KAB. BALANGAN</h3>
+				<p class="text-muted">Formulir Elektronik IKM (Indeks Kepuasan Masyarakat) Badan Kepegawaian Pendidikan dan Pelatihan Daerah Kabupaten Balangan.</p>
 				<a href="<?= base_url('survei?card=bkpsdm_balangan') ?>" class="btn btn-block btn-secondary btn-lg px-4 shadow">
 					Lanjutkan
 					<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 20 20">
