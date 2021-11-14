cf41646b0bc99c5f6243bfb6f5e8bfe2ccb492e6
muhammadnorseputra <muhammadnorseputra@gmail.com>
2021-11-14 11:42:46 +0700

    up security

--- application/config/config.php
+++ application/config/config.php
@@ -436,7 +436,7 @@ $config['standardize_newlines'] = false;
 |          for backwards compatibility purposes!
 |
 */
-$config['global_xss_filtering'] = TRUE;
+$config['global_xss_filtering'] = false;
 
 /*
 |--------------------------------------------------------------------------
