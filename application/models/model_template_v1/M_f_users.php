fd8a52a2e252f3963622bac8b1b4c4836957991f
muhammadnorseputra <muhammadnorseputra@gmail.com>
2021-11-15 11:45:26 +0700

    up badwords

--- application/models/model_template_v1/M_f_users.php
+++ application/models/model_template_v1/M_f_users.php
@@ -360,7 +360,7 @@ class M_f_users extends CI_Model
     // return $this->db->like('word', $word)->get('t_badwords');
     $this->db->select('word');
     $this->db->from('t_badwords');
-    $this->db->where('word', $word);
+    $this->db->like('word', $word);
     $q = $this->db->get();
     return $q;
   }
