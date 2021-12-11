7410d6d9dc2b3f6d9436dae4f12dbf2904844acc
muhammadnorseputra <muhammadnorseputra@gmail.com>
2021-12-11 21:25:43 +0700

    up loading

--- application/views/Frontend/v1/layout/head.php
+++ application/views/Frontend/v1/layout/head.php
@@ -54,9 +54,9 @@
 
 <body>
 <div class="page-slider">
-    <div class="logo-loading">
+    <!-- <div class="logo-loading">
         <img class="w-50" src="<?= base_url('assets/images/logo.png'); ?>" alt="BKPPD BALANGAN">
-    </div> 
+    </div>  -->
   <div class="slider">
     <div class="line"></div>
     <div class="subline inc"></div>
