cff1fc0c2d1648e09c4b883c1f96a51f45d47947
muhammadnorseputra <muhammadnorseputra@gmail.com>
2021-12-25 11:47:06 +0700

    up home

--- application/views/Frontend/v1/function/slider4.php
+++ application/views/Frontend/v1/function/slider4.php
@@ -3,13 +3,11 @@
     <div class="row d-flex align-items-center">
         <div class="col-md-6 py-md-5 mt-5 order-last order-md-first text-left">
             <div class="mt-md-5">
-                <h2 class="text-white font-weight-bold animated fadeInUp">Portal WEB Resmi Badan Kepegawaian dan Pengembangan Sumber Daya Manusia</h2>
-                <p class="text-warning animated fadeInUp">Selamat Datang Pada Portal Web Resmi BKPSDM Kabupaten Balangan, Pelayanan Terpadu.</p>
+            	<p class="text-warning animated fadeIn lead">Selamat Datang</p>
+                <h2 class="text-white font-weight-bold animated fadeIn">Portal WEB Resmi Badan Kepegawaian dan Pengembangan Sumber Daya Manusia</h2>
+                <p class="text-warning animated fadeIn">Kami senantiasa melayani dengan sepetuh hati, pelayanan terpadu satu pintu. <i>We are proud to be able to serve you :)</i></p>
             </div>
         </div>
     </div>
 </div>
 </section>
-<!-- <div>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 190"><path fill="#09a79e" fill-opacity="1" d="M0,192L26.7,165.3C53.3,139,107,85,160,53.3C213.3,21,267,11,320,10.7C373.3,11,427,21,480,42.7C533.3,64,587,96,640,106.7C693.3,117,747,107,800,90.7C853.3,75,907,53,960,58.7C1013.3,64,1067,96,1120,122.7C1173.3,149,1227,171,1280,176C1333.3,181,1387,171,1413,165.3L1440,160L1440,0L1413.3,0C1386.7,0,1333,0,1280,0C1226.7,0,1173,0,1120,0C1066.7,0,1013,0,960,0C906.7,0,853,0,800,0C746.7,0,693,0,640,0C586.7,0,533,0,480,0C426.7,0,373,0,320,0C266.7,0,213,0,160,0C106.7,0,53,0,27,0L0,0Z"></path></svg>
-</div> -->
