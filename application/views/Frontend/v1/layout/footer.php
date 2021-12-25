c98ca00ea2d038ee68f09555906e935306ed95ca
muhammadnorseputra <muhammadnorseputra@gmail.com>
2021-12-25 22:49:04 +0700

    pull req

--- application/views/Frontend/v1/layout/footer.php
+++ application/views/Frontend/v1/layout/footer.php
@@ -151,7 +151,7 @@
 	</div> 
 </section>
 </footer>
-<script async src="<?= base_url('template/v1/prod/vendor-min.js'); ?>"></script>
-<script async src="<?= base_url('template/v1/prod/app-min.js'); ?>"></script>
+<script defer src="<?= base_url('template/v1/prod/vendor-min.js'); ?>"></script>
+<script defer src="<?= base_url('template/v1/prod/app-min.js'); ?>"></script>
 </body>
 </html>
