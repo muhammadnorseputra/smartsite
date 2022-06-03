add22cc82b6a39efd1f1a72a2277de56fc084c84
Purtabungsu <muhammadnorseputra@gmail.com>
2022-06-03 10:03:34 +0800

    fix navbar

--- template/v1/prod/app.js
+++ template/v1/prod/app.js
@@ -855,13 +855,13 @@ $(function () {
 $(document).ready(function () {
   $(document).scroll(function () {
     if ($(document).scrollTop() > 80) {
-      $("nav#navbar").removeClass('navbar-dark').css("transition", ".3s ease-in-out").addClass("bg-blur navbar-light shadow-sm");
+      /*$("nav#navbar").removeClass('navbar-dark').css("transition", ".3s ease-in-out").addClass("bg-blur navbar-light");*/
       $("button#caripost").addClass('btn-outline-light');
     } else {
       $("button#caripost").removeClass('btn-outline-light');
       /*$("button#caripost").toggleClass('');*/
 
-      $("nav#navbar").addClass('navbar-dark').removeClass("bg-blur navbar-light shadow-sm");
+      /*$("nav#navbar").addClass('navbar-dark').removeClass("bg-blur navbar-light");*/
     }
   });
   /*
