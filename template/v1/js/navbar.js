801526d328a6126ca77a8c2f142233fd95657f10
Purtabungsu <muhammadnorseputra@gmail.com>
2022-06-03 09:17:13 +0800

    up navbar

--- template/v1/js/navbar.js
+++ template/v1/js/navbar.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
     // make it as accordion for smaller screens
     $(document).scroll(function() {
         if ($(document).scrollTop() > 80) {
-            $("nav#navbar").removeClass('navbar-dark').css("transition", ".2s ease-in-out").addClass("bg-blur navbar-light shadow-sm");
+            $("nav#navbar").removeClass('navbar-dark').css("transition", ".5s ease-in-out").addClass("bg-blur navbar-light");
             $("button#caripost").addClass('btn-outline-light');
         } else {
             $("button#caripost").removeClass('btn-outline-light');
             // $("button#caripost").toggleClass('');
-            $("nav#navbar").addClass('navbar-dark').removeClass("bg-blur navbar-light shadow-sm");
+            $("nav#navbar").addClass('navbar-dark').removeClass("bg-blur navbar-light");
         }
     });
     // var prevScrollpos = window.pageYOffset;
