e3d82f83c49fee3518078fbe03a5bae1c6e0efe9
Purtabungsu <muhammadnorseputra@gmail.com>
2022-06-03 08:56:35 +0800

    by pc all in one

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -25,7 +25,7 @@ class ComposerAutoloaderInitf57d80892b9eda248217e8be194dfbac
         require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInitf57d80892b9eda248217e8be194dfbac', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitf57d80892b9eda248217e8be194dfbac', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,51 +1,50 @@
-<?php return array (
-  'root' => 
-  array (
-    'pretty_version' => 'dev-master',
-    'version' => 'dev-master',
-    'aliases' => 
-    array (
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'df89a2feaf5cc5d3dd5cb96d3d36fd004d3b0799',
+        'name' => '__root__',
+        'dev' => true,
     ),
-    'reference' => '7bd8e9671fe631b073c14525c404df5e3af2c644',
-    'name' => '__root__',
-  ),
-  'versions' => 
-  array (
-    '__root__' => 
-    array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '7bd8e9671fe631b073c14525c404df5e3af2c644',
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'df89a2feaf5cc5d3dd5cb96d3d36fd004d3b0799',
+            'dev_requirement' => false,
+        ),
+        'chriskacerguis/codeigniter-restserver' => array(
+            'pretty_version' => '3.1.4',
+            'version' => '3.1.4.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../chriskacerguis/codeigniter-restserver',
+            'aliases' => array(),
+            'reference' => 'f386f4e0013daadaa213681a0a41af69aa81d9de',
+            'dev_requirement' => false,
+        ),
+        'league/event' => array(
+            'pretty_version' => '2.2.0',
+            'version' => '2.2.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../league/event',
+            'aliases' => array(),
+            'reference' => 'd2cc124cf9a3fab2bb4ff963307f60361ce4d119',
+            'dev_requirement' => false,
+        ),
+        'paulredmond/html-to-amp' => array(
+            'pretty_version' => 'v0.1.1',
+            'version' => '0.1.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../paulredmond/html-to-amp',
+            'aliases' => array(),
+            'reference' => '7062ce7d78ea066c87cc52fecd6710e0082bf80c',
+            'dev_requirement' => false,
+        ),
     ),
-    'chriskacerguis/codeigniter-restserver' => 
-    array (
-      'pretty_version' => '3.1.1',
-      'version' => '3.1.1.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '6ced70d12c7e8d207eaded3f208a201330950b57',
-    ),
-    'league/event' => 
-    array (
-      'pretty_version' => '2.2.0',
-      'version' => '2.2.0.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'd2cc124cf9a3fab2bb4ff963307f60361ce4d119',
-    ),
-    'paulredmond/html-to-amp' => 
-    array (
-      'pretty_version' => 'v0.1.1',
-      'version' => '0.1.1.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '7062ce7d78ea066c87cc52fecd6710e0082bf80c',
-    ),
-  ),
 );
