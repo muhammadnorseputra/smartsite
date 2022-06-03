d87dfd8cfdb1a0aaeb847e8a97e3a78cde5c2d3f
Purtabungsu <muhammadnorseputra@gmail.com>
2022-06-03 09:19:23 +0800

    up navbar

--- template/v1/js/navbar.js
+++ template/v1/js/navbar.js
@@ -1,23 +1,26 @@
 $(document).ready(function() {
-    // make it as accordion for smaller screens
+    
     $(document).scroll(function() {
         if ($(document).scrollTop() > 80) {
             $("nav#navbar").removeClass('navbar-dark').css("transition", ".5s ease-in-out").addClass("bg-blur navbar-light");
             $("button#caripost").addClass('btn-outline-light');
         } else {
             $("button#caripost").removeClass('btn-outline-light');
-            // $("button#caripost").toggleClass('');
+            /*$("button#caripost").toggleClass('');*/
             $("nav#navbar").addClass('navbar-dark').removeClass("bg-blur navbar-light");
         }
     });
-    // var prevScrollpos = window.pageYOffset;
-    // window.onscroll = function() {
-    //     var currentScrollPos = window.pageYOffset;
-    //     if (prevScrollpos > currentScrollPos) {
-    //         document.getElementById("navbar").style.top = "0";
-    //     } else {
-    //         document.getElementById("navbar").style.top = "-80px";
-    //     }
-    //     prevScrollpos = currentScrollPos;
-    // }
+    /*
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById("navbar").style.top = "0";
+        } else {
+            document.getElementById("navbar").style.top = "-80px";
+        }
+        prevScrollpos = currentScrollPos;
+    
+    }
+    */
 });
