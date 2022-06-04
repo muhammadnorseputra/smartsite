e089aaff755e6c249f3da6c9152b0e919a3aff58
muhammadnorseputra <muhammadnorseputra@gmail.com>
2022-06-04 14:59:56 +0700

    up slider banner

--- template/v1/prod/app.js
+++ template/v1/prod/app.js
@@ -712,29 +712,29 @@ $(function () {
   if (h > 17 && h < 24) $("span#halojs").text("Selamat Malam,");
   if (h > 23 || h < 4) $("span#halojs").text('Sekarang Jam  ' + h + ':' + m);
 });
-// $(function() {
-//     // $(document).scroll(function() {
-//     //     if ($(document).scrollTop() > 650) {
-//     //         if (!$.cookie("notice-accepted")) {
-//     //             $("a#xbanner-1").click();
-//     //             $.cookie("notice-accepted", 1, {
-//     //                 expires: 60 / 1440,
-//     //                 path: '/'
-//     //             });
-//     //         }
-//     //     }
-//     // });
-//     var urlChannel = 'https://www.youtube.com/channel/UCFDRHqqNeuYql8O7y5sHgmw?sub_confirmation=1';
-//     // var urlTo = `https://shrinkme.io/st?api=9168966d3b03eaf0daad63924162a46c98794cf0&url=${urlChannel}`;
-//     if (!$.cookie("shrinkme")) {
-//         window.open(urlTo, '_blank').focus();
-//         $.cookie("shrinkme", 1, {
-//             expires: 60 / 1440,
-//             path: '/'
-//         });
-//     }
-// });
 "use strict";
+
+$(function () {
+  $(document).scroll(function () {
+    if ($(document).scrollTop() > 650) {
+      if (!$.cookie("notice-accepted")) {
+        $("a#xbanner-1").click();
+        $.cookie("notice-accepted", 1, {
+          expires: 60 / 1440,
+          path: '/'
+        });
+      }
+    }
+  }); //     var urlChannel = 'https://www.youtube.com/channel/UCFDRHqqNeuYql8O7y5sHgmw?sub_confirmation=1';
+  //     // var urlTo = `https://shrinkme.io/st?api=9168966d3b03eaf0daad63924162a46c98794cf0&url=${urlChannel}`;
+  //     if (!$.cookie("shrinkme")) {
+  //         window.open(urlTo, '_blank').focus();
+  //         $.cookie("shrinkme", 1, {
+  //             expires: 60 / 1440,
+  //             path: '/'
+  //         });
+  //     }
+});
 // $(function() {
 //     $('h3#count_jml').countTo({
 //         refreshInterval: '50',
