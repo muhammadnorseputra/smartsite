4b87dee025bbebd9f124e33c08d75b87a1db35b2
Purtabungsu <muhammadnorseputra@gmail.com>
2022-06-13 10:52:23 +0800

    add dataku

--- application/views/Frontend/v1/function/search_pegawai.php
+++ application/views/Frontend/v1/function/search_pegawai.php
@@ -1,19 +1,25 @@
-<div class="separator">
-        <span class="separator-text text-uppercase font-weight-bold" style="background-color: #fff;"><i class="fas fa-user-alt text-secondary mr-2"></i> Search PNS</span>
-</div>
-<form class="form-horizontal shadow-sm" id="caripegawai" method="GET" action="<?= base_url('frontend/v1/pegawai/detail') ?>">
-    <div class="typeahead__container form-group text-left">
-        <!-- <label for="js-nipnama" class="text-muted pb-2 small">Search Pegawai</label>  -->
-        <div class="typeahead__field">
-            <div class="typeahead__query">
-                <input class="js-nipnama rounded" id="js-nipnama" name="filter[query]" placeholder="Masukan NIP" maxlength="18" autocomplete="off">
-            </div>
-            <!-- <div class="typeahead__button">
-                <button type="submit">
-                <i class="typeahead__search-icon"></i> Detail
-                </button>
-            </div> -->
+<div class="card  mt--8 mb-5">
+    <div class="card-body">
+    <div class="col-md-8">
+        <div class="separator">
+                <span class="separator-text text-uppercase font-weight-bold" style="background-color: #fff;"><i class="fas fa-user-alt text-secondary mr-2"></i> Search PNS</span>
         </div>
+        <form class="form-horizontal shadow-sm" id="caripegawai" method="GET" action="<?= base_url('frontend/v1/pegawai/detail') ?>">
+            <div class="typeahead__container form-group text-left">
+                <!-- <label for="js-nipnama" class="text-muted pb-2 small">Search Pegawai</label>  -->
+                <div class="typeahead__field">
+                    <div class="typeahead__query">
+                        <input class="js-nipnama rounded" id="js-nipnama" name="filter[query]" placeholder="Masukan NIP" maxlength="18" autocomplete="off">
+                    </div>
+                    <div class="typeahead__button">
+                        <button type="submit">
+                        <i class="typeahead__search-icon"></i> Detail
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </form>
+        Silahkan masukan Nomor Induk Pegawai (NIP) untuk melihat profile.
     </div>
-</form>
-Silahkan masukan Nomor Induk Pegawai (NIP) untuk melihat profile.
+    </div>
+</div>
---
+++ application/views/Frontend/v1/pages/pegawai/dataku.php
@@ -0,0 +1,20 @@
+<section class="hero py-md-5 pt-3">
+	<div class="container py-md-5">
+		<div class="d-flex justify-content-between align-items-start flex-lg-row flex-column">
+			<div>
+				<h3 class="font-weight-bold text-responsive text-white">CEK DATAKU </h3>
+				<p class="text-muted small">Resources <code>SILKa Online</code></p>
+			</div>
+		</div>
+	</div>
+</div>
+</section>
+<section>
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<?php $this->load->view('Frontend/v1/function/search_pegawai'); ?>
+			</div>
+		</div>
+	</div>
+</section>
