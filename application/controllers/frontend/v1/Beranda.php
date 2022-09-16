696b0cfbdb43ba3de09c779bb1faf607f5e06886
muhammadnorseputra <muhammadnorseputra@gmail.com>
2022-09-16 17:37:09 +0700

    style card post beranda

--- application/controllers/frontend/v1/Beranda.php
+++ application/controllers/frontend/v1/Beranda.php
@@ -214,10 +214,10 @@ class Beranda extends CI_Controller
                 $photo_terkait_sisa = $this->template_photo_terkait_sisa($total_sisa);
                 $photo_t = '';
                 if($photo_terkait->num_rows() > 0):
-                    $photo_t .= '<ul style="gap: 3px;" class="d-flex justify-content-between flex-wrap list-unstyled rounded-top m-0 pr-4">';
+                    $photo_t .= '<ul style="gap: 3px;" class="d-flex justify-content-start align-items-stretch flex-wrap list-unstyled rounded-top m-0 pr-4">';
                     foreach($photo_terkait->result() as $p):
                         $photo_t .= '<li style="max-width: 180px; max-height: 160">
-                                        <img class="lazy" data-src="'.img_blob($p->photo).'" alt="'.$p->judul.'" style="object-fit: contain;"/>
+                                        <img class="lazy w-100 h-100" data-src="'.img_blob($p->photo).'" alt="'.$p->judul.'" style="object-fit: contain;"/>
                                     </li>';
                     endforeach;
                     $photo_t .= ' </ul>';
