bd124d2fdf15fa3a97db56d817521cd611b1c7c5
putrabungsu <muhammadnorseputra@gmail.com>
2022-12-22 09:23:39 +0700

    up req

--- template/v1/prod/app.js
+++ template/v1/prod/app.js
@@ -463,86 +463,27 @@ function batal() {
   $(".emojionearea-editor").removeClass('is-invalid').addClass('is-valid');
   $(".reply_username").attr('id', '').html('');
 }
-// const CONTAINER = $("#countpeg_container");
-// const URL = 'http://silka.balangankab.go.id'
-
-// function Loading(isLoading) {
-//     if(isLoading) {
-//     return `<div class="col-6 col-md-3 p-2">
-//                 <span class="content-placeholder rounded-circle mx-auto d-block" style="width:65px; height: 65px;">&nbsp;</span>
-//                 <span class="content-placeholder my-1" style="width: 100%; height: 60px;"></span>
-//                 <span class="content-placeholder" style="width: 100%; height: 30px;"></span>
-//             </div>
-//             <div class="col-6 col-md-3 p-2">
-//                 <span class="content-placeholder rounded-circle mx-auto d-block" style="width:65px; height: 65px;">&nbsp;</span>
-//                 <span class="content-placeholder my-1" style="width: 100%; height: 60px;"></span>
-//                 <span class="content-placeholder" style="width: 100%; height: 30px;"></span>
-//             </div>
-//             <div class="col-6 col-md-3 p-2">
-//                 <span class="content-placeholder rounded-circle mx-auto d-block" style="width:65px; height: 65px;">&nbsp;</span>
-//                 <span class="content-placeholder my-1" style="width: 100%; height: 60px;"></span>
-//                 <span class="content-placeholder" style="width: 100%; height: 30px;"></span>
-//             </div>
-//             <div class="col-6 col-md-3 p-2">
-//                 <span class="content-placeholder rounded-circle mx-auto d-block" style="width:65px; height: 65px;">&nbsp;</span>
-//                 <span class="content-placeholder my-1" style="width: 100%; height: 60px;"></span>
-//                 <span class="content-placeholder" style="width: 100%; height: 30px;"></span>
-//             </div>`;
-//     }
-// }
-
-// CONTAINER.html(Loading(true));
-
-// fetch(`${URL}/api/get_grap_all`, {
-//     method: 'GET',
-//     referrerPolicy: "strict-origin-when-cross-origin",
-//   })
-// .then(response => response.json())
-// .then((res) => {
-//     Loading(false);
-//     CONTAINER.html(`
-//     <div class="col-6 col-sm-6 col-md-3 rounded-left">
-//         <div class="card bg-transparent border-0 rounded">
-//             <div class="card-body text-center">
-//                 <i class="fas fa-users pb-md-4 rounded fa-3x text-info"></i>
-//                 <h1 id="count_jml" class="font-weight-bold" data-speed="3000">${res.asn}</h1>
-//                 <b class="text-secondary small">Jumlah ASN Kab. Balangan</b>
-//             </div>
-//         </div>
-//     </div>
-//     <div class="col-6 col-sm-6 col-md-3">
-//         <div class="card bg-transparent border-0 rounded">
-//             <div class="card-body text-center">
-//                 <i class="fas fa-user-tie pb-md-4 fa-3x mx-auto text-success rounded"></i>
-//                 <h1 id="count_jml" data-speed="3000" class="font-weight-bold">${res.pns}</h1>
-//                 <b class="text-secondary small">Jumlah PNS + CPNS</b>
-//             </div>
-//         </div>
-//     </div>
-//     <div class="col-6 col-sm-6 col-md-3 rounded-right">
-//         <div class="card bg-transparent rounded border-0">
-//             <div class="card-body text-center">
-//                 <i class="far pb-md-4 fa-user-circle fa-3x mx-auto text-warning rounded"></i>
-//                 <h1 id="count_jml" data-speed="3000" class="font-weight-bold">${res.nonpns}</h1>
-//                 <b class="text-secondary small">Jumlah NON PNS (Pendataan thn. 2018)</b>
-//             </div>
-//         </div>
-//     </div>
-//     <div class="col-6 col-sm-6 col-md-3 rounded-right">
-//         <div class="card bg-transparent rounded border-0">
-//             <div class="card-body text-center">
-//                 <i class="fas pb-md-4 fa-home fa-3x mx-auto text-secondary rounded"></i>
-//                 <h1 id="count_jml" data-speed="3000" class="font-weight-bold">${res.pensiun}</h1>
-//                 <b class="text-secondary small">Jumlah PNS Pensiun</b>
-//             </div>
-//         </div>
-//     </div>
-//     `);
-// })
-// .catch((error) => {
-//     console.error(error)
-// })
 "use strict";
+
+var CONTAINER = $("#countpeg_container");
+var URL = 'http://silka.balangankab.go.id';
+function Loading(isLoading) {
+  if (isLoading) {
+    return "<div class=\"col-6 col-md-3 p-2\">\n                <span class=\"content-placeholder rounded-circle mx-auto d-block\" style=\"width:65px; height: 65px;\">&nbsp;</span>\n                <span class=\"content-placeholder my-1\" style=\"width: 100%; height: 60px;\"></span>\n                <span class=\"content-placeholder\" style=\"width: 100%; height: 30px;\"></span>\n            </div>\n            <div class=\"col-6 col-md-3 p-2\">\n                <span class=\"content-placeholder rounded-circle mx-auto d-block\" style=\"width:65px; height: 65px;\">&nbsp;</span>\n                <span class=\"content-placeholder my-1\" style=\"width: 100%; height: 60px;\"></span>\n                <span class=\"content-placeholder\" style=\"width: 100%; height: 30px;\"></span>\n            </div>\n            <div class=\"col-6 col-md-3 p-2\">\n                <span class=\"content-placeholder rounded-circle mx-auto d-block\" style=\"width:65px; height: 65px;\">&nbsp;</span>\n                <span class=\"content-placeholder my-1\" style=\"width: 100%; height: 60px;\"></span>\n                <span class=\"content-placeholder\" style=\"width: 100%; height: 30px;\"></span>\n            </div>\n            <div class=\"col-6 col-md-3 p-2\">\n                <span class=\"content-placeholder rounded-circle mx-auto d-block\" style=\"width:65px; height: 65px;\">&nbsp;</span>\n                <span class=\"content-placeholder my-1\" style=\"width: 100%; height: 60px;\"></span>\n                <span class=\"content-placeholder\" style=\"width: 100%; height: 30px;\"></span>\n            </div>";
+  }
+}
+CONTAINER.html(Loading(true));
+fetch("".concat(URL, "/api/get_grap_all"), {
+  method: 'GET',
+  referrerPolicy: "strict-origin-when-cross-origin"
+}).then(function (response) {
+  return response.json();
+}).then(function (res) {
+  Loading(false);
+  CONTAINER.html("\n    <div class=\"col-6 col-sm-6 col-md-3 rounded-left\">\n        <div class=\"card bg-transparent border-0 rounded\">\n            <div class=\"card-body text-center\">\n                <i class=\"fas fa-users pb-md-4 rounded fa-3x text-info\"></i>\n                <h1 id=\"count_jml\" class=\"font-weight-bold\" data-speed=\"3000\">".concat(res.asn, "</h1>\n                <b class=\"text-secondary small\">Jumlah ASN Kab. Balangan</b>\n            </div>\n        </div>\n    </div>\n    <div class=\"col-6 col-sm-6 col-md-3\">\n        <div class=\"card bg-transparent border-0 rounded\">\n            <div class=\"card-body text-center\">\n                <i class=\"fas fa-user-tie pb-md-4 fa-3x mx-auto text-success rounded\"></i>\n                <h1 id=\"count_jml\" data-speed=\"3000\" class=\"font-weight-bold\">").concat(res.pns, "</h1>\n                <b class=\"text-secondary small\">Jumlah PNS + CPNS</b>\n            </div>\n        </div>\n    </div>\n    <div class=\"col-6 col-sm-6 col-md-3 rounded-right\">\n        <div class=\"card bg-transparent rounded border-0\">\n            <div class=\"card-body text-center\">\n                <i class=\"far pb-md-4 fa-user-circle fa-3x mx-auto text-warning rounded\"></i>\n                <h1 id=\"count_jml\" data-speed=\"3000\" class=\"font-weight-bold\">").concat(res.nonpns, "</h1>\n                <b class=\"text-secondary small\">Jumlah NON PNS (Pendataan thn. 2018)</b>\n            </div>\n        </div>\n    </div>\n    <div class=\"col-6 col-sm-6 col-md-3 rounded-right\">\n        <div class=\"card bg-transparent rounded border-0\">\n            <div class=\"card-body text-center\">\n                <i class=\"fas pb-md-4 fa-home fa-3x mx-auto text-secondary rounded\"></i>\n                <h1 id=\"count_jml\" data-speed=\"3000\" class=\"font-weight-bold\">").concat(res.pensiun, "</h1>\n                <b class=\"text-secondary small\">Jumlah PNS Pensiun</b>\n            </div>\n        </div>\n    </div>\n    "));
+})["catch"](function (error) {
+  console.error(error);
+});
 "use strict";
 
 $(document).ready(function () {
