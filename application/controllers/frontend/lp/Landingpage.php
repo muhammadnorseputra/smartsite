94c491031741185d4bc119ab87cae34513e7c9b6
Muhammad Nor Seputra <43113176+muhammadnorseputra@users.noreply.github.com>
2023-08-22 10:26:09 +0700

    fix 3

--- application/controllers/frontend/lp/Landingpage.php
+++ application/controllers/frontend/lp/Landingpage.php
@@ -17,7 +17,7 @@ class Landingpage extends CI_Controller {
             $v = 'satu.php';
         } elseif($slug === 'slot-dana') {
 			$v = 'dua.php';
-		} elseif($slug === 'slot-gacor-live-rtp') {
+		} elseif($slug === 'slot-rtp') {
 			$v = 'tiga.php';
 		} elseif($slug === 'slot-gacor-maxwin') {
 			$v = 'empat.php';
