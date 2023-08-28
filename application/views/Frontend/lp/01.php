5c7a861a95295d017526cd161b1de617ce21a7ed
Muhammad Nor Seputra <43113176+muhammadnorseputra@users.noreply.github.com>
2023-08-28 17:36:43 +0700

    up 01

---
+++ application/views/Frontend/lp/01.php
@@ -0,0 +1,192 @@
+
+<!doctype html>
+<html ⚡ lang="id" dir="ltr">
+<head>
+<meta charset="utf-8">
+<title>Madu303 - Situs Judi Slot Online, Bola, Poker 88 Dan Togel Serta Live Casino Terpercaya</title>
+<meta content="Madu303 Adalah Agen Judi Slot Online Casino Yang Menyediakan 7 Provider Terbaik Diantaranya Bola Atau Parlay, Live Casino, Slot Game, Tembak Ikan, Poker Hingga Togel dan Balap Kuda." name="description">
+<meta content="Madu303,Judi Online, Judi Slot Online, Bola Parlay, Poker 88, Madu303.com, Madu 303, Live Casino, Slot Game, Tembak Ikan, Togel, Balap Kuda" name="keywords">
+<link href="https://bkpsdm.balangankab.go.id/store/madu303" rel="canonical">
+<link rel="icon" href="/images/icon.png" type="image/x-icon">
+<meta name="robots" content="index,follow">
+<script async src="https://cdn.ampproject.org/v0.js"></script>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1,maximum-scale=1,user-scalable=no">
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style" content="black">
+<meta property="og:locale" content="id_ID" />
+<meta property="og:url" content="https://bkpsdm.balangankab.go.id/store/madu303" />
+<meta property="og:type" content="website" />
+<meta property="og:image" content="https://stingkai.website/images/banner.jpg" />
+<meta property="og:title" content="Madu303 - Situs Judi Slot Online, Bola, Poker 88 Dan Togel Serta Live Casino Terpercaya" />
+<meta property="og:description" content="Madu303.Com Adalah Agen Judi Slot Online Casino Yang Menyediakan 7 Provider Terbaik Diantaranya Bola Atau Parlay, Live Casino, Slot Game, Tembak Ikan, Poker Hingga Togel dan Balap Kuda." />
+<style amp-custom>.myButton {
+	-moz-box-shadow: 0px 0px 0px 0px #052b3b;
+	-webkit-box-shadow: 0px 0px 0px 0px #052b3b;
+	box-shadow: 0px 0px 0px 0px #052b3b;
+	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #33bdef), color-stop(1, #286985));
+	background:-moz-linear-gradient(top, #DAA520  5%, #000000 100%);
+	background:-webkit-linear-gradient(top, #DAA520  5%, #000000 100%);
+	background:-o-linear-gradient(top, #DAA520  5%, #000000 100%);
+	background:-ms-linear-gradient(top, #DAA520  5%, #000000 100%);
+	background:linear-gradient(to bottom, #DAA520  5%, #000000 100%);
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#33bdef', endColorstr='#286985',GradientType=0);
+	background-color:#33bdef;
+	-moz-border-radius:6px;
+	-webkit-border-radius:6px;
+	border-radius:6px;
+	border:1px solid #082130;
+	display:inline-block;
+	cursor:pointer;
+	color:#fff;
+	font-family:Georgia;
+	font-size:25px;
+	font-weight:bold;
+	padding:9px 35px;
+	text-decoration:none;
+	text-shadow:0px -1px 0px #f4f5f7;
+}
+.myButton:hover {
+	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #286985), color-stop(1, #33bdef));
+	background:-moz-linear-gradient(top, #000000 5%, #DAA520 100%);
+	background:-webkit-linear-gradient(top, #000000 5%, #DAA520 100%);
+	background:-o-linear-gradient(top, #000000 5%, #DAA520 100%);
+	background:-ms-linear-gradient(top, #000000 5%, #DAA520 100%);
+	background:linear-gradient(to bottom, #000000 5%, #DAA520 100%);
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#286985', endColorstr='#33bdef',GradientType=0);
+	background-color:#286985;
+}
+.myButton:active {
+	position:relative;
+	top:1px;
+}
+
+}
+#footer{height: 60px; min-width: 100%;}
+#menu-footer, #menu ul{margin: 0; padding: 0; list-style: none;}
+#menu-footer{margin: 0px 0 5px 0; }
+#menu-footer li{display: inline-block; padding: 0 5px; border-left: 1px solid #EFE390;}
+#menu-footer li:first-child{border: none;}
+#menu-footer li a{color: #fff;}
+body{
+background: #000 url(/images/bg.jpg)
+                }
+                </style>
+	<style amp-boilerplate>
+                body {
+                    -webkit-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+                    -moz-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+                    -ms-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+                    animation: -amp-start 8s steps(1, end) 0s 1 normal both
+                }
+                
+                @-webkit-keyframes -amp-start {
+                    from {
+                        visibility: hidden
+                    }
+                    to {
+                        visibility: visible
+                    }
+                }
+                
+                @-moz-keyframes -amp-start {
+                    from {
+                        visibility: hidden
+                    }
+                    to {
+                        visibility: visible
+                    }
+                }
+                
+                @-ms-keyframes -amp-start {
+                    from {
+                        visibility: hidden
+                    }
+                    to {
+                        visibility: visible
+                    }
+                }
+                
+                @-o-keyframes -amp-start {
+                    from {
+                        visibility: hidden
+                    }
+                    to {
+                        visibility: visible
+                    }
+                }
+                
+                @keyframes -amp-start {
+                    from {
+                        visibility: hidden
+                    }
+                    to {
+                        visibility: visible
+                    }
+                }
+            </style>
+            <noscript>
+                <style amp-boilerplate>
+                    body {
+                        -webkit-animation: none;
+                        -moz-animation: none;
+                        -ms-animation: none;
+                        animation: none
+                    }
+                </style>
+            </noscript>
+</head>
+<body>
+
+<center>
+<div style="border:1px solid #34egt; box-shadow: 0 0 1px 1px #34egt; border-radius: 10px;background: rgba(0, 0, 0, 0.4) none repeat scroll 0% 0%; padding:5px 10px;">
+			<center>
+			<amp-img alt="logo" src="https://stingkai.website/images/logo/madu303.png" title="Madu303" width="300" height="100"></amp-img><br />
+			<a class="myButton" href="#" target="_blank" rel="nofollow">DAFTAR ID</a>
+			<a class="myButton" href="#" target="_blank" rel="nofollow">LOGIN AKUN</a><br />
+			<h1 style="font-weight:bold; font-family: Georgia; font-size:18px; color:#fff;">Madu303 - Situs Judi Slot Online, Bola, Poker 88 Dan Togel Serta Live Casino Terpercaya</h1>
+			<p style="text-align: justify;font-family: Georgia;font-size:16px; color:#fff;">Madu303.Com Adalah Bandar <a href="http://192.64.115.161/~default/madu303/"><b>Agen Judi Slot Online</b></a> Dengan 7 Provider Judi Online Terpercaya Yang Menyediakan Berbagai Jenis Permainan Diantaranya Judi Slot Online, Bola Parlay, Poker 88, Live Casino, Slot Game, Tembak Ikan, Togel, Balap Kuda.</p><br>
+			<amp-img alt="Poker Madu303" src="/images/Poker88.png" title="Poker88" height="100" width="100"></amp-img>
+			<amp-img alt="Slot Madu303" src="/images/Slot.png" title="Slot" height="100" width="100"></amp-img>
+			<amp-img alt="Togel Madu303" src="/images/Togel.png" title="Togel" height="100" width="100"></amp-img>
+			<amp-img alt="Parlay Madu303" src="/images/Parlay.png" title="Parlay" height="100" width="100"></amp-img>
+			<amp-img alt="TembakIkan Madu303" src="/images/TembakIkan.png" title="TembakIkan" height="100" width="100"></amp-img>
+			<amp-img alt="Bonus Madu303" src="/images/Bonus.png" title="Bonus" height="100" width="100"></amp-img>
+			<amp-img alt="Promo Madu303" src="/images/Promo.png" title="Promo" height="100" width="100"></amp-img>
+			<amp-img alt="LiveCasino Madu303" src="/images/LiveCasino.png" title="LiveCasino" height="100" width="100"></amp-img>
+			<br>
+			<h1 style="font-weight:bold; font-family: Georgia; font-size:18px; color:#fff;">Madu303 - Link Alternatif Resmi, Login dan Daftar Madu303</h1>
+			<p style="text-align: justify;font-family: Georgia;font-size:16px; color:#fff;">hallo pecinta poker 88 dan slot online, saat ini telah hadir <a href="http://192.64.115.161/~default/madu303/">Situs poker 88</a> tempatnya pemenang yaitu situs Madu303, di jamin hoki bermain melalui situs Madu303, selain itu Madu303 juga menawarkan berbagai promo menarik untuk anda yang telah menjadi bagian dari Madu303 ^-^.</p>
+			<h2 style="font-weight:bold; font-family: Georgia; font-size:18px; color:#fff;">Download Apk Madu303 Android Versi Mobile</h2>
+			<p style="text-align: justify;font-family: Georgia;font-size:16px; color:#fff;">Download applikasi <a href="http://192.64.115.161/~default/madu303/">Slot Online</a> versi lama dan terbaru yang sangat mobile smartphone android dan ios anda, sebagai salah satu situs bandarq online terbaik dan selain mengandalkan Promo Banjir Bonus, Madu303 juga memfasilitasi secara serius dan lengkap mengenai kebutuhan pilihan provider judi oleh para member Madu303, Madu303.com sebagai Situs <b>Judi Online</b> akan selalu update jika ada pemainan baru yg dilaunching oleh pihak provider, untuk itu para member tidak perlu khawatir akan ketinggalan uvoria bermain permainan terbaru.</p>
+			<h3 style="font-weight:bold; font-family: Georgia; font-size:18px; color:#fff;">Website Madu 303 DI Lengkapi Id Pro Serta Banjir Bonus Untuk New Member</h3>
+			<p style="text-align: justify;font-family: Georgia;font-size:16px; color:#fff;">Daftar <a href="http://192.64.115.161/~default/madu303/">Id Pro Slot Online</a> Madu303 resmi melalui link alternatif terbaru disini, untuk bonus kami telah mempersiapkan miliaran rupiah untuk mengisi kantong anda yang berhasil memenangkan hadiah jackpot games slot Online maupun poker88, dan para member Madu 303 akan di manjakan dengan bonus x2 dari total hadiah jackpot yang dimenangkan, selain itu Madu303 juga menyediakan bonus referal sebesar 0,3% untuk para pegiat media yang membagikan link Madu303 88  ke teman-temannya, juga bonus turn over menarik untuk para member yang kalah dalam laga bertanding di idn poker  live, setiap bonus akan Madu 303 bagikan setiap minggu dan otomatis masuk ke id masing-masing member.</p>
+			<br>
+			<amp-img alt="BRI" src="https://stingkai.website/images/bri.png" title="bank bri" height="30" width="50"></amp-img>
+			<amp-img alt="Mandiri" src="https://stingkai.website/images/mandiri.png" title="bank mandiri" height="30" width="50"></amp-img>
+			<amp-img alt="BNI" src="https://stingkai.website/images/bni.png" title="bank bni" height="30" width="50"></amp-img>
+			<amp-img alt="BCA" src="https://stingkai.website/images/bca.png" title="bank bca" height="30" width="50"></amp-img>
+			<amp-img alt="DANAMON" src="https://stingkai.website/images/danamon.png" title="bank danamon" height="30" width="50"></amp-img>
+			<amp-img alt="Aplikasi Madu303" src="https://stingkai.website/images/android.png" title="Madu303 Aplikasi" height="30" width="300"></amp-img>
+			<br>
+			<div id="footer">
+			<div class="wraper">
+			<div style="float: center"></div>
+
+			<div style="float: center">
+			<ul id="menu-footer">
+				<li><a href="<?= base_url("store/madu303") ?>">Situs Slot Online</a></li>
+				<li><a href="<?= base_url("store/madu303") ?>">Agen Judi Slot</a></li>
+				<li><a href="<?= base_url("store/madu303") ?>">Bola Parlay</a></li>
+				<li><a href="<?= base_url("store/madu303") ?>">Bandar Poker 88</a></li>
+				<li><a href="<?= base_url("store/madu303") ?>">Live Casino Idn</a></li>
+			</ul>
+			<p style="color: #fff; text-align: center;">Hak Cipta © Madu303</p>
+			</div>
+			</div>
+			</div>
+			</center>
+</div>
+</center>
+</body>
+</html>
